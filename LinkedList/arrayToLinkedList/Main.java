562b1b262e06bce0f640b7245b901198bff69c99
SaiAvinash-23 <saiavinashn@gwmail.gwu.edu>
2026-02-06 06:11:03 -0500

    Linked list basic operations

--- LinkedList/arrayToLinkedList/Main.java
+++ LinkedList/arrayToLinkedList/Main.java
@@ -1,5 +1,45 @@
 package LinkedList.arrayToLinkedList;
 
 public class Main {
-    
+    public static Node arrayToLinkedList(int[] arr) {
+        int size = arr.length;
+        if (size == 0) return null;
+
+        Node head = new Node(arr[0]);
+        Node current = head;
+
+        for (int i = 1; i < size; i++) {
+            current.next = new Node(arr[i]);
+            current = current.next;
+            System.out.println(current);
+        }
+
+        return head;
+    }
+
+    public static void printLinkedList(Node head) {
+        Node current = head;
+        while(current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        Node head = arrayToLinkedList(arr);
+
+        printLinkedList(head);
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int val) {
+        this.data = val;
+        this.next = null;
+    }
 }
