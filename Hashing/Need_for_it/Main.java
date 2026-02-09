2aeeb39b8c9cb67d2b43a06c2d45c0bcc2e027d7
SaiAvinash-23 <saiavinashn@gwmail.gwu.edu>
2026-02-09 14:51:49 -0500

    longest sequence in the array

--- Hashing/Need_for_it/Main.java
+++ Hashing/Need_for_it/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-
         ArrayList<Integer> arr = new ArrayList<>();
+        int[] queryArray = {5, 200, 415, 1000, 1569, 1832};
 
         System.out.println("Enter the size of the array: ");
         int originalLength = sc.nextInt();
@@ -15,7 +15,7 @@ public class Main {
         while(len != 0) {
             // System.out.println("Enter number: " + (originalLength - len + 1));
             // int input = sc.nextInt();
-            int input = rand.nextInt(2000 - 0 + 1) + 0; 
+            int input = rand.nextInt(15 - 0 + 1) + 0; 
             arr.add(input);
             len--;
         }
@@ -23,7 +23,18 @@ public class Main {
         ArrayList<Integer> hashArr = createHash(arr);
         System.out.println("Array Length: " + arr.size());
         System.out.println("Hash Array Length: " + hashArr.size());
-        System.out.println("Hash Array: " + hashArr.toString());
+        System.out.println("Array: " + arr.toString() + "\n");
+        System.out.println("Hash Array: " + hashArr.toString() + "\n");
+
+        // for (int i = 0; i < queryArray.length; i++) {
+        //     System.out.printf("Count of %d in the array: %d", queryArray[i], countOccurences(arr, queryArray[i]));
+        //     System.out.println();
+        // }
+        for (int i = 0; i < queryArray.length; i++) {
+            System.out.println("Count of " + queryArray[i] + " : " + countOccurenceHash(hashArr, queryArray[i]));
+        }
+
+
         sc.close();
     }
 
@@ -55,4 +66,55 @@ public class Main {
         
         return hashArr;
     }
+
+    // Only if array is used.
+    public static int countOccurences(ArrayList<Integer> arr, int target) {
+        int count = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if(arr.get(i) == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // if hash is used
+    public static int countOccurenceHash(ArrayList<Integer> hashArr, int target) {
+        if (target < 0 || target > hashArr.size()) {
+            return 0;
+        }
+        int num = Math.min(100, 10);
+        System.out.println(num);
+        int count = hashArr.get(target);
+        return count != 0 ? count : 0;
+
+    }
 }
+
+
+
+
+// class Solution {
+//     public int mostFrequentElement(int[] nums) {
+//         if (nums.length == 0) return -1;
+
+//         int maxOccuring = 1;
+//         int maxOccuringNum = nums[0];
+//         for (int i = 0; i < nums.length; i++) {
+//             int count = 0;
+//             for (int j = 0; j < nums.length; j++) {
+//                 if (nums[j] == nums[i]) {
+//                     count++;
+//                 }
+//             }
+//             if (count > maxOccuring) {
+//                 maxOccuring = count;
+//                 maxOccuringNum = nums[i];
+//             } else if (count == maxOccuring) {
+//                 maxOccuringNum = Math.min(nums[i], maxOccuringNum);
+//             }
+//         }
+
+//         return maxOccuringNum;
+//     }
+// }
---
+++ Hashing/longestSequence/Main.java
@@ -0,0 +1,17 @@
+package Hashing.longestSequence;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+
+        Solution sol = new Solution();
+
+        System.out.println("Longest sequence in the array: " + sol.longestConsective(nums));
+    }
+}
+
+class Solution {
+    public int longestConsective(int[] nums) {
+        return 0;
+    }
+}
